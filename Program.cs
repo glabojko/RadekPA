ad337549405698f99f073401974dcd2335ac4984
glabojko <glabojko@gmail.com>
2023-09-21 23:47:01 +0200

    kids added

--- Program.cs
+++ Program.cs
@@ -4,19 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // var nowy = new Employee();
-            Human nowychop = new("greg", "labojko", "male", 46);
-            Console.WriteLine(nowychop.ToString());
-            Employee nowyPracownik = new("greg", "labojko", "male", 46, 30, "cap", "manango", 500.3, 3);
-            Console.WriteLine(nowyPracownik.ToString());
-            nowyPracownik.ChangeName("dupa");
-            Console.WriteLine(nowyPracownik.ToString());
-            nowyPracownik.Rise();
-            Console.WriteLine(nowyPracownik.ToString());
-            Console.WriteLine(nowychop.ToString());
-            Console.WriteLine(nowychop.Metoda());
-            Console.WriteLine(nowyPracownik.Metoda());
+            Console.WriteLine("Hello, and welcome to Labojko's company!");
+            
+            Human Chris = new("Chris", "Labojko", "male", 19);
+            Human Peter = new("Peter", "Labojko", "male", 18);
+            Human Wojtek = new("Wojtek", "Labojko", "male", 15);
+            Human Adam = new("Adam", "Labojko", "male", 13);
+            Human Anna = new("Anna", "Labojko", "male", 9);
+            Console.WriteLine(Chris.ToString());
+            Console.WriteLine(Peter.ToString());
+            Console.WriteLine(Wojtek.ToString());
+            Console.WriteLine(Adam.ToString());
+            Console.WriteLine(Anna.ToString());
+            Employee ChrisEmployee = new("Chris", "Labojko", "male", 19, 20, "The Labojko's Company", "manango", 825.5, 2);
+            Console.WriteLine(ChrisEmployee.ToString());
+            ChrisEmployee.ChangeName("dupa");
+            //Console.WriteLine(nowyPracownik.ToString());
+            //nowyPracownik.Rise();
+            //Console.WriteLine(nowyPracownik.ToString());
+            //Console.WriteLine(nowychop.ToString());
+            //Console.WriteLine(nowychop.Metoda());
+            //Console.WriteLine(nowyPracownik.Metoda());
 
         }
     }
--- Specialist.cs
+++ Specialist.cs
@@ -17,5 +17,7 @@ namespace RadekPA
         {
 
         }
+
+
     }
 }
